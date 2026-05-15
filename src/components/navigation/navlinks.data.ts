5282043d2f3ea54f5d6c470597a9e05d1ce15232
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-15 22:23:14 +0530

    Added data for Nav links with - path & label

---
+++ src/components/navigation/navlinks.data.ts
@@ -0,0 +1,7 @@
+import type { NAVLINKS_PROPS } from "./navlinks.types";
+
+export const NAVLINKS_DATA: NAVLINKS_PROPS[] = [
+    { path: '/', label: 'Home' },
+    { path: '/about', label: 'About' },
+    { path: '/docs', label: 'Docs' },
+]
