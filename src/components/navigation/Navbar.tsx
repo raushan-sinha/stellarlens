19620b2cf4c7654df9b59b57c5e2902033723fed
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-15 22:56:01 +0530

    Added icons - Menu & Cross

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,5 +1,7 @@
 import { NAVLINKS_DATA } from "./navlinks.data";
 import SatelliteAltIcon from '@mui/icons-material/SatelliteAlt';
+import MenuIcon from '@mui/icons-material/Menu';
+import ClearIcon from '@mui/icons-material/Clear';
 
 export default function Navbar() {
     return (
@@ -22,12 +24,9 @@ export default function Navbar() {
                 </div>
 
                 {/* Mobile Menu Button */}
-                <button
-                    className="md:hidden text-gray-600 hover:text-gray-900 focus:outline-none"
-                >
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
+                <button className="md:hidden text-gray-600 hover:text-gray-900 focus:outline-none">
+                    <MenuIcon />
+                    <ClearIcon />
                 </button>
             </div>
 
